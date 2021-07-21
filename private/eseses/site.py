cfd631420568101f8ee496b5845476cd5146ed81
Angelyn W Moore <awmoore@jpl.nasa.gov>
2021-07-20 18:07:09 -0700

    Print message in site info when model terms are unavailable, for clarity

--- private/eseses/site.py
+++ private/eseses/site.py
@@ -81,6 +81,11 @@ def getModelTerms(site,f,source,fil):
                 modelTerms['North']['RMS'].append(li.split(':')[1].split(',')[0] + ' mm')
                 modelTerms['East']['RMS'].append(li.split(':')[1].split(',')[1] + ' mm')
                 modelTerms['Up']['RMS'].append(li.split(':')[1].split(',')[2] + ' mm')
+            elif 'available' in li:
+                modelTerms['North']['Model']=['Not available']
+                modelTerms['East']['Model']=['Not available']
+                modelTerms['Up']['Model']=['Not available']
+         
     return(modelTerms)
 
 allModelTerms = OrderedDict()
