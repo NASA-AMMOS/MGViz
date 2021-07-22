2db7ae55b38ac025e29b07f1a4af32dd47f02fd5
Angelyn W Moore <awmoore@jpl.nasa.gov>
2021-07-21 17:14:45 -0700

    source .bashrc to get right LD_LIBRARY_PATH

--- private/eseses/sioCron.sh
+++ private/eseses/sioCron.sh
@@ -1,5 +1,12 @@
 #crontab -e
 #0 0 * * * ~/ESESES/private/eseses/cron.sh
+source ~/.bashrc
 cd ~/ESESES/private/eseses
 python metadata.py
 python timeseries.py
+# These are (?) run from within server.js
+#cd ~/ESESES
+#python private/eseses/psite.py
+#python private/eseses/velocity.py jpl
+#python private/eseses/velocity.py sopac
+#python private/eseses/velocity.py comb
