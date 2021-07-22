1f3bfc7a7b0bb95722906fe4386c821d203940a3
Angelyn W Moore <awmoore@jpl.nasa.gov>
2021-07-22 10:01:31 -0700

    Remove commented lines running psite.py and velocity.py.  These are run from within server.js.  Earlier failure to update the json files from within the app was probably caused by the web service move to port 8080.

--- private/eseses/sioCron.sh
+++ private/eseses/sioCron.sh
@@ -4,9 +4,3 @@ source ~/.bashrc
 cd ~/ESESES/private/eseses
 python metadata.py
 python timeseries.py
-# These are (?) run from within server.js
-#cd ~/ESESES
-#python private/eseses/psite.py
-#python private/eseses/velocity.py jpl
-#python private/eseses/velocity.py sopac
-#python private/eseses/velocity.py comb
